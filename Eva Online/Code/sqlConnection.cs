b5cb83e3e83d59688b593b335eeada6167f5b031
Wizzy <tudor.andrei.604@gmail.com>
2021-01-28 11:11:09 +0200

    connection

---
+++ Eva Online/Code/sqlConnection.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eva_Online.Code
+{
+    public static class sqlConnection
+    {
+        public static string sqlConnectionString = @"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+
+        public static SqlConnection con = new SqlConnection(sqlConnectionString);
+
+        public static bool Start()
+        {
+            try
+            {
+                con.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public static string[] FetchData(string query)
+        {
+            try
+            {
+
+                SqlDataAdapter SqlAD = new SqlDataAdapter();
+                SqlAD.SelectCommand = new SqlCommand(query, con);
+                DataSet ds = new DataSet();
+                SqlAD.Fill(ds);
+
+                DataTable dt = ds.Tables[0];
+
+                if (dt.Rows.Count != 0)
+                {
+                    string[] arr = new string[dt.Rows[0].ItemArray.Length];
+
+                    arr = InitializeArrays.InitStringArray(arr);
+
+
+                    DataRow row = dt.Rows[0]; //first array
+
+                    for (int i = 0; i < dt.Rows[0].ItemArray.Length; i++)
+                    {
+                        arr[i] = row[i].ToString();
+
+                    }
+                    return arr;
+                }
+            }
+            catch (SqlException Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+            return null;
+        }
+
+        public static int Execute(string query)
+        {
+            try
+            {
+                SqlCommand com = new SqlCommand(query, con);
+                return com.ExecuteNonQuery();
+            }
+            catch (SqlException Ex)
+            {
+                MessageBox.Show("MSSQL Error -> Function exec(" + query + "), exception catched : " + Ex.ToString());
+                return 0;
+            }
+        }
+    }
+
+    class InitializeArrays
+    {
+        public static string[] InitStringArray(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = "";
+            }
+            return arr;
+        }
+
+        public static int[] InitIntArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 0;
+            }
+            return arr;
+        }
+
+        public static bool[] InitBoolArray(bool[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = false;
+            }
+            return arr;
+        }
+    }
+}
--- Eva Online/Forms/AdministrationForm.cs
+++ Eva Online/Forms/AdministrationForm.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static Eva_Online.Code.sqlConnection;
 using System.Windows.Forms;
+using Eva_Online.Code;
 
 namespace Eva_Online.Forms
 {
@@ -15,6 +9,72 @@ namespace Eva_Online.Forms
         public AdministrationForm()
         {
             InitializeComponent();
+            sqlConnection.Start();
+
+            this.Load += (sender, e) => FormLoad();
+
+        }
+
+        private void InsertToGrid(int id)
+        {
+            dataGridView1.Rows.Clear();
+            int qNumber = int.Parse(FetchData("exec Intrebari..getQuestionsCount '" + id + "'")[0]);
+            for (int i = 1; i <= qNumber; i++)
+            {
+                string[] data = FetchData("USE Intrebari select * from QuestionDefinition where TestID = '" + id + "' and QuestionID = '" + i + "'");
+                dataGridView1.Rows.Add(data);
+
+            }
+
+        }
+
+        private void InsertToComboBox(int id)
+        {
+            string testName = FetchData($"exec Intrebari..getTestName '{id}'")[0];
+
+            comboBoxTests.Items.Add(testName);
+        }
+
+        private void FormLoad()
+        {
+            int numberOfTests = int.Parse(FetchData("exec Intrebari..getTestsNumber")[0]);
+            for (int i = 1; i <= numberOfTests; i++)
+            {
+                InsertToComboBox(i);
+            }
+
+
+            comboBoxTests.SelectedValueChanged += (sender, e) => InsertToGrid(comboBoxTests.SelectedIndex + 1);
+
+            buttonAddText.Click += (snder, e) =>
+            {
+                foreach (DataGridViewRow r in dataGridView1.Rows)
+                {
+                    try
+                    {
+
+                        string[] data = new string[]
+                        {
+                        r.Cells[0].Value.ToString(),
+                        r.Cells[1].Value.ToString(),
+                        r.Cells[2].Value.ToString(),
+                        r.Cells[3].Value.ToString(),
+                        r.Cells[4].Value.ToString(),
+                        r.Cells[5].Value.ToString(),
+                        r.Cells[6].Value.ToString(),
+                        r.Cells[7].Value.ToString(),
+                        r.Cells[8].Value.ToString()
+                        };
+
+                        Execute("Use intrebari delete from QuestionDefinition where TestID = '" + data[0] + "' and QuestionID = '" + data[1] + "'");
+                        Execute($"Use intrebari insert into QuestionDefinition(TestID, QuestionID, QuestionText,AnswerType,Answer1,Answer2,Answer3,Answer4,CorrectAnswer)" +
+                            $" values ('{data[0]}','{data[1]}','{data[2]}','{data[3]}','{data[4]}','{data[5]}','{data[6]}','{data[7]}','{data[8]}')");
+                    }
+                    catch { }
+
+
+                }
+            };
         }
     }
 }
