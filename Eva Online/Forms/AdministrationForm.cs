63a842419af68e0fcf979c856cb75a85a2c41b4f
EpicWings <pasca_robert2003@yahoo.com>
2021-01-28 08:53:48 +0200

    pt 1

--- Eva Online/Forms/AdministrationForm.cs
+++ Eva Online/Forms/AdministrationForm.cs
@@ -8,16 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Eva_Online
+namespace Eva_Online.Forms
 {
-    public partial class Form1 : Form
+    public partial class AdministrationForm : Form
     {
-        public Form1()
+        public AdministrationForm()
         {
             InitializeComponent();
-            //sakfew
-            //iashn cf09upashfcjasdfasdf
         }
-
     }
 }
